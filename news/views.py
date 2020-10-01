a0dc4b5d4c91acff6ca1a36cdca56efc6ff76f78
Jeremy Phetphoumy <jeremy.phetphoumy@openevents.fr>
2020-10-01 19:10:09 +0200

    Fix set caching to 30

--- news/views.py
+++ news/views.py
@@ -23,7 +23,7 @@ def fetchNews(url):
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
     # Cache request
-    requests_cache.install_cache(cache_name='newsapi_cache', backend='sqlite', expire_after=3600)
+    requests_cache.install_cache(cache_name='newsapi_cache', backend='sqlite', expire_after=1800)
     user_agent_list = [
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
