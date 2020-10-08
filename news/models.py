66631d48ce43e18587e78bb365e935b27fb0abc0
Pratyush <pratyush.19b131054@abes.ac.in>
2020-10-08 13:28:46 +0530

    Revert "Patch 7 (#10)" (#11)
    This reverts commit 6520284294efbdacf71cbc5dbdd661d67969979c.

--- news/models.py
+++ news/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class New(models.Model):
-    """
-    Model class for New
-    """
-    title = models.CharField(max_length=255)
-    tag = models.CharField(max_length=5)
-
